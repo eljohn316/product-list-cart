e938f13d92b890fbaf0f55b847c4080bc1d6ee26
El John Bonga <bongaeljohn07@gmail.com>
2026-09-23 21:11:25 +0800

    chore: setup app design system

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from '@/App.vue'
+import '@fontsource-variable/red-hat-text';
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from '@/App.vue';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
+app.use(createPinia());
 
-app.mount('#app')
+app.mount('#app');
